37552b326d2d2f9ab70187769782282647a226e3
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-02-03 19:41:35 +0100

    refactor: move all protocols imports into a single index file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, jest, test } from "@jest/globals";
 import { SignUpController } from "./signup";
 import { MissingParamError, InvalidParamError, ServerError } from "../errors";
-import { EmailValidator } from "../protocols/email-validator";
+import { EmailValidator } from "../protocols";
 
 interface SutTypes {
   sut: SignUpController;
