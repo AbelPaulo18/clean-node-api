08333587700997bed10d7208ca24d2c5a7fe2034
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-02-04 00:53:29 +0100

    refactor: ensure signUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -228,4 +228,26 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test("should return 200 if valid data is provided", () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: "valid-name",
+        email: "valid-email@mail.com",
+        password: "valid-password",
+        passwordConfirmation: "valid-password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "validId",
+      name: "valid-name",
+      email: "valid-email@mail.com",
+      password: "valid-password",
+    });
+  });
 });
